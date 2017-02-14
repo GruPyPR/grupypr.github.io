5f67f3b161d7456ac2c5c7e053b3a0acdb9c235d
Ellison Leão <ellisonleao@gmail.com>
2017-02-14 14:36:36 -0200

    adding analytics and disqus values. Resolve #13

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "grupypr"
+GOOGLE_ANALYTICS = "UA-899823-19"
