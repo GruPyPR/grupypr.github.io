0710cc099802a956b044e299efd2dc487af788ba
Thiago Serafim <thiago.serafim@gmail.com>
2017-03-07 00:11:29 -0300

    adiciona artigo sobre seundo encontro de 2017

--- pelicanconf.py
+++ pelicanconf.py
@@ -68,6 +68,12 @@ MEDIUS_AUTHORS = {
             ('twitter-square', 'https://twitter.com/turicas'),
         )
     },
+    'Thiago Serafim': {
+        'links': (
+            ('github-square', 'https://github.com/tserafim'),
+            ('twitter-square', 'https://twitter.com/thiagoserafim'),
+        )
+    },
     'William Souza': {
         'image': 'https://avatars3.githubusercontent.com/u/215986?v=3&s=150',
         'links': (
